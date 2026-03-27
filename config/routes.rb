8de722737c202b6953bb266bcf0817f4af8e9d0e
cami <lamica.olivares@gmail.com>
2026-03-27 15:37:27 -0400

    products router

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get "products/index"
 
   get "/products", to: "products#index"
-  
+
+  get "/products/:id", to: "products#show"
+
+
   post "/products", to: "products#create"
 
   
