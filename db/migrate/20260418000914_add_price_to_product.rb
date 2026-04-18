06a67e4f34981bc9a57dcd893a558cabe2a36b59
cami <lamica.olivares@gmail.com>
2026-04-17 20:25:13 -0400

    add column db

---
+++ db/migrate/20260418000914_add_price_to_product.rb
@@ -0,0 +1,5 @@
+class AddPriceToProduct < ActiveRecord::Migration[7.2]
+  def change
+    add_column :products, :price, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_31_213737) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_18_000914) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_31_213737) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "price"
   end
 
   create_table "users", force: :cascade do |t|
